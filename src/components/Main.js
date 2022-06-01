7cff0b89dd5682adf68e5baed84ddf91dfe5b92b
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-01 14:46:04 +0300

    feat: добавить обработчики к кнопкам в профиле

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,20 @@
 function Main() {
 
+  function handleEditAvatarClick() {
+    const popup = document.querySelector('.popup_type_change-avatar');
+    popup.classList.add('popup_opened');
+  }
+
+  function handleEditProfileClick() {
+    const popup = document.querySelector('.popup_type_edit-profile');
+    popup.classList.add('popup_opened');
+  }
+
+  function handleAddPlaceClick() {
+    const popup = document.querySelector('.popup_type_add-card');
+    popup.classList.add('popup_opened');
+  }
+
   return (
     <main>
 
@@ -11,16 +26,25 @@ function Main() {
             class="profile__avatar-image" />
           <button class="profile__avatar-button"
             type="button"
-            aria-label="Обновить аватар"></button>
+            aria-label="Обновить аватар"
+            onClick={handleEditAvatarClick}></button>
         </div>
         <div class="profile__info">
           <div class="profile__name-block">
             <h1 class="profile__name">. . .</h1>
-            <button type="button" class="profile__button profile__button_type_edit" aria-label="Редактировать профиль"></button>
+            <button
+              type="button"
+              class="profile__button profile__button_type_edit"
+              aria-label="Редактировать профиль"
+              onClick={handleEditProfileClick}></button>
           </div>
           <p class="profile__job"></p>
         </div>
-        <button type="button" class="profile__button profile__button_type_add" aria-label="Добавить фотографию"></button>
+        <button
+          type="button"
+          class="profile__button profile__button_type_add"
+          aria-label="Добавить фотографию"
+          onClick={handleAddPlaceClick}></button>
       </section>
 
       <section class="cards content__element" aria-label="Фотографии">

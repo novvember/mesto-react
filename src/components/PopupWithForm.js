8b25b8782528311d7e5f7bf05c43a7106014f7ee
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-06-01 22:45:41 +0300

    fix: исправить во всех файлах novalidate на noValidate

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm(props) {
     <div className={`popup popup_type_${props.name}` + (props.isOpen && ' popup_opened')}>
       <div className="popup__container content__element">
         <h2 className="popup__title">{props.title}</h2>
-        <form className="popup__form" name={props.name} novalidate>
+        <form className="popup__form" name={props.name} noValidate>
           {props.children}
           <button className="popup__save-button" type="submit">{props.buttonText}</button>
         </form>
